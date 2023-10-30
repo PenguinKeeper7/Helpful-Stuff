149aebb7cf61c6318183d8584a0f3a4d1fcb4225
PenguinKeeper7 <64717060+PenguinKeeper7@users.noreply.github.com>
2023-10-30 17:36:23 +0000

    Add Custom Charset support
    Add support for custom charsets, ?1 / ?2 / ?3 / ?4

--- Tools/keyspaceLimiter.py
+++ Tools/keyspaceLimiter.py
@@ -1,59 +1,100 @@
 import codecs, sys
 
+customCharsets = [0, 0, 0, 0]
+
+def calculateKeyspace(character):
+	if(character == "?"):
+		return 1
+	elif (character == "l"):
+		return 26
+	elif (character == "u"):
+		return 26
+	elif (character == "d"):
+		return 10
+	elif (character == "h"):
+		return 16
+	elif (character == "H"):
+		return 16
+	elif (character == "s"):
+		return 33
+	elif (character == "a"):
+		return 95
+	elif (character == "b"):
+		return 256
+	elif (character == "1"):
+		return customCharsets[0]
+	elif (character == "2"):
+		return customCharsets[1]
+	elif (character == "3"):
+		return customCharsets[2]
+	elif (character == "4"):
+		return customCharsets[3]
+	else:
+		return 1;
+
+# TO-DO: Prevent duplicate entries from increasing keyspace, like Hashcat does
+def prepareCustomCharsets(mask):
+	if (',' not in mask):
+		return
+		
+	global customCharsets
+	customCharsets = [0, 0, 0, 0]
+	
+	index = 0
+	
+	# Remove actual mask section, only keep the custom charset declarations
+	for i in mask.split(',')[:-1]:
+		for character in i:
+			if (character != '?'):
+				customCharsets[index] += calculateKeyspace(character)
+		index += 1
+
 args = sys.argv
 
 try:
-    #Attempt to open the file with the first argument
-    maskFile = codecs.open(args[1], "r", encoding="utf8", errors="replace")
+	#Attempt to open the file with the first argument
+	maskFile = codecs.open(args[1], "r", encoding="utf8", errors="replace")
 
-    #Enforce the keyspace is numeric, then set to second argument
-    keyspaceMax = 0
-    if(args[2].isnumeric()):
-        keyspaceMax = int(args[2])
-    else: a
-    
-    #Check if the keyspace should be printed
-    printKeyspace = False
-    if(len(args) == 4):
-        if (args[3].lower() == "true"):
-            printKeyspace = True
-            
+	#Enforce the keyspace is numeric, then set to second argument
+	keyspaceMax = 0
+	if(args[2].isnumeric()):
+		keyspaceMax = int(args[2])
+	else: a
+	
+	#Check if the keyspace should be printed
+	printKeyspace = False
+	if(len(args) == 4):
+		if (args[3].lower() == "true"):
+			printKeyspace = True
+			
 except:
-    print("Please enter the correct argument format:")
-    print("keyspaceLimiter.py maskFile maxKeyspace [true] \n") 
-    sys.exit()
+	print("Please enter the correct argument format:")
+	print("keyspaceLimiter.py maskFile maxKeyspace [true] \n") 
+	sys.exit()
 
-for mask in maskFile:
-    #Reset variables
-    keyspace = 1
-    
-    for i in range(len(mask)):
-        character = mask[i]
+for originalMask in maskFile:
+	keyspace = 1
 
-        if (mask[max(i - 1, 0)] != "?"):
-            continue
-        elif (character == "l"):
-            keyspace *= 26
-        elif (character == "u"):
-            keyspace *= 26
-        elif (character == "d"):
-            keyspace *= 10
-        elif (character == "h"):
-            keyspace *= 16
-        elif (character == "H"):
-            keyspace *= 16
-        elif (character == "s"):
-            keyspace *= 33
-        elif (character == "a"):
-            keyspace *= 95
-        elif (character == "b"):
-            keyspace *= 256
+	# Ensure custom charset keyspace values are assigned correctly
+	prepareCustomCharsets(originalMask)
+	
+	# Remove custom charset section of mask (if applicable)
+	mask = originalMask.rsplit(',',)[-1]
+	
+	for i in range(len(mask)):
+		character = mask[i]
 
-    #If the mask doesn't exceed the max keyspace
-    if(keyspace <= keyspaceMax):
-        if(printKeyspace):
-            print(mask.rstrip() + ":" + str(keyspace))
-        else:
-            print(mask.rstrip())
+		# Ensure the character previously was a ?
+		if (mask[max(i - 1, 0)] != "?"):
+			continue
+			
+		keyspace *= calculateKeyspace(character)
+
+	# Don't output if the mask doesn't exceed the max keyspace
+	if(keyspace <= keyspaceMax):
+		if(printKeyspace):
+			print(originalMask.rstrip() + ":" + str(keyspace))
+		else:
+			print(originalMask.rstrip())
 
 maskFile.close()
